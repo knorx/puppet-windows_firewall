f9564c5b57db85d860a51019f753095edfe40be4
Ruben Stein <ruben.stein@mevis.fraunhofer.de>
2020-03-09 16:27:49 +0100

    ensure existence of parent keys

--- manifests/exception.pp
+++ manifests/exception.pp
@@ -168,8 +168,12 @@ define windows_firewall::exception(
       $netsh_command = "${netsh_exe} advfirewall firewall ${fw_action} rule name=\"${display_name}\" ${fw_description} dir=${direction} action=${action} enable=${mode} edge=${edge} ${allow_context} remoteip=\"${remote_ip}\""
     }
     #
-    registry_value { 'HKLM\Software\Puppet Labs\Puppet\modules\windows_firewall\netsh_command':
+    registry_key { 'HKLM\Software\Puppet Labs\Puppet\modules':
       ensure => $ensure,
+    }
+    ~> registry_value { 'netsh_command':
+      ensure => $ensure,
+      key    => 'HKLM\Software\Puppet Labs\Puppet\modules\windows_firewall',
       type   => string,
       data   => $netsh_command,
     }
