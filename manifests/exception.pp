65c228b973b0ed6b678fcadee9b7ee365259df82
Ruben Stein <ruben.stein@mevis.fraunhofer.de>
2020-03-10 11:47:35 +0100

    identify rules only by name when deleting

--- manifests/exception.pp
+++ manifests/exception.pp
@@ -159,15 +159,21 @@ define windows_firewall::exception(
       false => 'no',
     }
 
+    $netsh_command_rule_name = "rule name=\"${display_name}\""
     $netsh_command_front = "${netsh_exe} advfirewall firewall"
-    $netsh_command_shared = "rule name=\"${display_name}\" dir=${direction}"
+    $netsh_command_shared = "dir=${direction}"
     $netsh_command_program = "action=${action} enable=${mode} edge=${edge}"
     $netsh_command_back = "${allow_context} remoteip=\"${remote_ip}\""
-    if $fw_action == 'delete' and $program == undef {
-      $netsh_command = "${netsh_command_front} ${fw_action} ${fw_description} ${netsh_command_shared} ${netsh_command_back}"
+    if $fw_action == 'delete' {
+      $netsh_command = "${netsh_command_front} ${fw_action} ${netsh_command_rule_name}"
     } else {
-      $netsh_command = "${netsh_command_front} ${fw_action} ${fw_description} ${netsh_command_shared} ${netsh_command_program} ${netsh_command_back}"
-      $netsh_delete_command = "${netsh_command_front} delete ${netsh_command_shared} ${netsh_command_program} ${netsh_command_back}"
+      if $program == undef{
+        $netsh_command = "${netsh_command_front} ${fw_action} ${fw_description} ${netsh_command_rule_name} ${netsh_command_shared} ${netsh_command_back}"
+      }
+      else {
+        $netsh_command = "${netsh_command_front} ${fw_action} ${fw_description} ${netsh_command_rule_name} ${netsh_command_shared} ${netsh_command_program} ${netsh_command_back}"
+      }
+      $netsh_delete_command = "${netsh_command_front} delete ${netsh_command_rule_name}"
     }
     #
     registry_key { 'HKLM\Software\Puppet Labs\Puppet\modules':
